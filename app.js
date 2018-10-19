59ae40597e79f9c81f4b0389467ea66f2c387a4e
92181abc <chenhanzhong@sunmi.com>
2018-10-20 00:43:24 +0800

    again

--- app.js
+++ app.js
@@ -13,16 +13,8 @@ var router = require('./routes/index')
 var config = require('./config/default')
 
 const MongoStore = connectMongo(session)
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials','true');
-  next();
-};
 
 var app = express();
-app.use(allowCrossDomain);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,7 +37,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-console.log(router, 9766)
 router(app)
 // app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,14 @@ import users from './users'
 
 /* GET home page. */
 
-
+router.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 module.exports = app =>{
   app.use('/', router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
